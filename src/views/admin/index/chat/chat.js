88be3b99313fdc030637f07160a82a93138bbe87
PhamDuy-k3 <phamdoduy123@gmail.com>
2024-06-20 14:24:53 +0700

    chat file

--- src/views/admin/index/chat/chat.js
+++ src/views/admin/index/chat/chat.js
@@ -5,6 +5,7 @@ import "./ChatRealTime.scss";
 const ChatRealTime = () => {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
+  const [imageFile, setImageFile] = useState(null); // State để lưu trữ file ảnh
   const socketRef = useRef(null);
 
   useEffect(() => {
@@ -12,7 +13,6 @@ const ChatRealTime = () => {
     socketRef.current = socket;
 
     socket.on("message", (msg) => {
-      console.log(msg);
       setMessages((prevMessages) => [
         ...prevMessages,
         { ...msg, sender: "server" },
@@ -23,6 +23,7 @@ const ChatRealTime = () => {
       socket.disconnect();
     };
   }, []);
+
   const formatTime = (timestamp) => {
     const date = new Date(timestamp);
     return date.toLocaleTimeString([], {
@@ -31,36 +32,77 @@ const ChatRealTime = () => {
       hour12: false,
     });
   };
-  const sendMessage = () => {
-    if (socketRef.current) {
+
+  const handleFileInputChange = (event) => {
+    const file = event.target.files[0];
+    setImageFile(file);
+  };
+
+  const sendTextMessage = () => {
+    if (message.trim() !== "") {
       const messageWithTimestamp = {
         text: message,
         timestamp: new Date().toISOString(),
         sender: "self",
       };
-      socketRef.current.emit("message", messageWithTimestamp.text);
+      socketRef.current.emit("message", messageWithTimestamp);
       setMessages((prevMessages) => [...prevMessages, messageWithTimestamp]);
       setMessage("");
     }
   };
+
+  const sendImageMessage = () => {
+    if (imageFile) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const messageWithTimestamp = {
+          type: "image",
+          data: event.target.result,
+          timestamp: new Date().toISOString(),
+          sender: "self",
+        };
+        socketRef.current.emit("message", messageWithTimestamp);
+        setMessages((prevMessages) => [...prevMessages, messageWithTimestamp]);
+        setImageFile(null); // Reset imageFile state after sending
+      };
+      reader.readAsDataURL(imageFile);
+    }
+  };
+
+  const handleSendMessage = () => {
+    if (imageFile) {
+      sendImageMessage();
+    } else {
+      sendTextMessage();
+    }
+  };
+
   const messagesList = messages.map((msg, index) => (
     <div key={index} className={`message ${msg.sender}`}>
-      <div>{msg.text}</div>
-      <div className={`timestamp-hidden ${msg.sender}`}>
-        {formatTime(msg.timestamp)}
-      </div>
+      {msg.type === "image" ? (
+        <img
+          style={{ width: "10rem", height: "10rem" }}
+          src={msg.data}
+          alt="sent"
+        />
+      ) : (
+        <div>{msg.text}</div>
+      )}
+      <div className="timestamp-hidden">{formatTime(msg.timestamp)}</div>
     </div>
   ));
+
   return (
     <div className="chat-container">
       <div className="messages">{messagesList}</div>
       <div className="input-container">
+        <input type="file" onChange={handleFileInputChange} />
         <input
           type="text"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
-        <button onClick={sendMessage}>Send</button>
+        <button onClick={handleSendMessage}>Send</button>
       </div>
     </div>
   );
